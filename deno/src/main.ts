0d990ce524bc748c3f7c8933f7469c227ff5ca95
Yuval Saraf <unimonkiez@gmail.com>
2024-10-29 00:17:53 +0100

    Working python

--- deno/src/main.ts
+++ deno/src/main.ts
@@ -1,4 +1,5 @@
 import * as mx from "./schema.generated.ts";
+import { setTimeout } from "node:timers/promises";
 
 const memorix = new mx.Memorix();
 await memorix.connect();
@@ -8,9 +9,10 @@ await memorix.task.pass_ball.enqueue(mx.System.NODE, 0);
 
 await memorix.task.pass_ball.empty(mx.System.DENO);
 const gen = await memorix.task.pass_ball.dequeue(mx.System.DENO);
-
+await setTimeout(2_000);
 for await (const ball of gen.asyncIterator) {
   const biggerBall = ball + 1;
   console.log(`Passing the ball with value ${biggerBall}`);
   await memorix.task.pass_ball.enqueue(mx.System.NODE, biggerBall);
+  await setTimeout(2_000);
 }
